c1b20aab5d625d3674768625150cdf1af7eb93f8
Ansar <ansarsh1243@gmail.com>
2023-12-24 22:08:35 +0600

    Import

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+import models.Point;
+import models.Shape;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class MyApplication {
+    public static void main(String[] args) throws FileNotFoundException {
+        File file = new File("C:/Users/Ansar/Desktop/Java/assignment_intro/src/source.java");
+
+        Scanner sc = new Scanner(file);
+        Shape shape = new Shape();
+
+        while (sc.hasNext()) {
+            double x = sc.nextDouble();
+            double y = sc.nextDouble();
+
+            Point point = new Point(x, y);
+            shape.addPoint(point);
+        }
+
+        System.out.println(shape.calculatePerimeter());
+        System.out.println(shape.getLongest());
     }
 }
